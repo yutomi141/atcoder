4cccfcd09f43a5e22910abac29a2af6b0e6c782d
yutomi141 <yuto.mi1218816@gmail.com>
2025-10-17 22:06:53 +0900

    Add AtCoder Python templates

---
+++ templates.py/input.py
@@ -0,0 +1,18 @@
+x = int(input())
+
+
+n,x = map(int, input().split(' '))
+
+
+n = int(input())
+a = list(map(int, input().split(' ')))
+
+
+n = int(input())
+s=[]
+for i in range(n):
+    s.append(int(input()))
+
+
+n=int(input())
+pair = [tuple(map(int, input().split(' '))) for i in range(n)]
